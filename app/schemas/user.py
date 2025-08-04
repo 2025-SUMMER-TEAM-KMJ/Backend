44bdc2e5fdda05e9628435024bc59ca6f7909d93
JoonKyoLee <joonkyolee@gmail.com>
2025-08-05 00:42:36 +0900

    feat: 회원가입 및 응답용 UserCreate, UserResponse 스키마 추가

---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+# app/schemas/user.py
+from pydantic import BaseModel, EmailStr, constr
+from typing import Literal
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8)
+    name: str
+    age: int
+    gender: Literal["남성", "여성"]
+    phone: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+    age: int
+    gender: str
+    phone: str
+
+    class Config:
+        orm_mode = True

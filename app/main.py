725370152245ffe6cca6f03ef50585057eb85ab0
JoonKyoLee <joonkyolee@gmail.com>
2025-08-15 04:21:36 +0900

    feat: 채용 공고 조회, 추천, 북마크 관련 엔드포인트 추가

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.services.user import UserService
 from app.services.auth import AuthService
-from app.api.routers import user, auth
+from app.api.routers import user, auth, job_posting
 from app.database import init_db
 
 app = FastAPI()
@@ -54,3 +54,4 @@ async def shutdown_event():
 # 라우터 등록
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(job_posting.router)
